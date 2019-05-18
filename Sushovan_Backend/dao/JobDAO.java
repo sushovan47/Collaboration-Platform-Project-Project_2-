9905a38293f7b45d8b029cc9c6eefc209752fb39
sushovan47 <47148682+sushovan47@users.noreply.github.com>
2019-05-18 16:07:06 +0530

    update JobDAO with update method

--- Sushovan_Backend/dao/JobDAO.java
+++ Sushovan_Backend/dao/JobDAO.java
@@ -7,7 +7,8 @@ import com.coll.model.Job;
 public interface JobDAO 
 {
 	public boolean addJob(Job job);
-	public List<Job> showJobs(Job job); //Apply for job you can implement that is not mandatory.
+	public boolean updateJob(Job job);
+	public List<Job> showJobs();     //Apply for job you can implement that is not mandatory.
 	public Job getJob(int jobId);
 
 }
