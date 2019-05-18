dac0b0af1ef3e41f755a51d55ce19f1b48e51a49
sushovan47 <47148682+sushovan47@users.noreply.github.com>
2019-05-18 16:18:57 +0530

    updated RouteConfig

--- Sushovan_Frontend/RouteConfig.js
+++ Sushovan_Frontend/RouteConfig.js
@@ -1,4 +1,4 @@
-var myApp=angular.module("myApp",['ngRoute']);
+var myApp=angular.module("myApp",['ngRoute','ngCookies']);
 myApp.config(function($routeProvider)
 {	
 	alert("I am in RouteConfig Function");
@@ -6,7 +6,24 @@ myApp.config(function($routeProvider)
 $routeProvider.when("/",{templateUrl:"index.html"})
 
 .when("/login",{templateUrl:"c_user/Login.html"})
+.when("/register",{templateUrl:"c_user/Register.html"})
+.when("/addBlog",{templateUrl:"c_blog/AddBlog.html"})
+.when("/manageBlog",{templateUrl:"c_blog/AdminBlog.html"})
+.when("/showBlog",{templateUrl:"c_blog/ShowBlog.html"})
+.when("/showBlogComment",{templateUrl:"c_blog/showBlogComment.html"})
+.when("/userHome",{templateUrl:"c_user/UserHome.html"});
 
-.when("/register",{templateUrl:"c_user/Register.html"});
+});
 
+myApp.run(function($rootScope,$cookieStore)
+{
+	
+	console.log('I am in run function');
+	console.log($rootScope.currentUser);
+	
+	if($rootScope.currentUser==undefined)
+		{
+			console.log($cookieStore.get('userdetails'));
+			console.currentUser=$cookieStore.get('userdetails');
+		}
 });
