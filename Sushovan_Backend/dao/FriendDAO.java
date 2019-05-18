4f7adc9d0ac4b15676cb45fd5b55f1458611e934
sushovan47 <47148682+sushovan47@users.noreply.github.com>
2019-05-18 16:09:03 +0530

    updated

--- Sushovan_Backend/dao/FriendDAO.java
+++ Sushovan_Backend/dao/FriendDAO.java
@@ -1,15 +1,18 @@
 package com.coll.dao;
 
 import java.util.List;
-
 import com.coll.model.Friend;
+import com.coll.model.UserDetail;
 
 public interface FriendDAO
 {
-	public boolean addFriend(Friend friend);
-	public boolean deleteFriend(Friend friend);
+	public List<Friend> showFriendList(String loginName);
+	public List<Friend> showPendingFriendRequest(String loginName);
+	public  List<UserDetail> showSuggestedFriendRequest(String loginName);
 	
-	public Friend getFriend(String friendId);
-	public List<Friend> listFriend();
+	
+	public boolean sendFriendRequest(Friend friend);
+	public boolean acceptFriendRequest(int friendId);
+	public boolean deleteFriendRequest(int friendId);
 
 }
