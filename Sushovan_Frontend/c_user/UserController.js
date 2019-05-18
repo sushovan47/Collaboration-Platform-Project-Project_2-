398b6b5306f28397c9760d8bca0300454383e30d
sushovan47 <47148682+sushovan47@users.noreply.github.com>
2019-05-18 16:15:45 +0530

    updated

--- Sushovan_Frontend/c_user/UserController.js
+++ Sushovan_Frontend/c_user/UserController.js
@@ -1,12 +1,31 @@
-myApp.controller("UserController",function($scope)
+myApp.controller("UserController",function($scope,$location,$http,$rootScope)
 {
-	$scope.user={'username':'','password':'','memberName':'','EmailId':'','role':'','status':'','isOnline':''};
+	$scope.user={"username":"","password":"","memberName":"","EmailId":"","role":"","status":"","isOnline":""};
 	
 	$scope.loginCheck=function()
 	{
 		console.log("I am in Login Check");
 		console.log($scope.user.username);
 		console.log($scope.user.password);	
+		
+		$http.post("http://localhost:8081/Sushovan_Middleware/checkUser",$scope.user)
+		.then(function(response)
+		{
+			console.log('Logged In');
+			$scope.user=response.data;
+			$rootScope.currentUser=response.data;
+			console.log($rootScope.currentUser);
+			$cookieStore.put('userdetails',response.data);
+			console.log(response.data);
+			$location.path("/userHome");
+		});
+	}
+	$scope.logout=function()
+	{
+		console.log('I am in Logout Function');
+		delete $rootScope.currentUser;
+		$cookieStore.remove('userdetails');
+		$location.path("/login");
 	}
 	
 	$scope.register=function()
@@ -20,6 +39,13 @@ myApp.controller("UserController",function($scope)
 		
 		console.log($scope.user.memberName);
 		console.log($scope.user.emailId);
+		
+		$http.post("http://localhost:8081/Sushovan_Middleware/registerUser",$scope.user)
+		.then (function(response)
+		{
+			console.log('Registered');
+			$location.path("/Login");
+		});
 	}
 	
 	
