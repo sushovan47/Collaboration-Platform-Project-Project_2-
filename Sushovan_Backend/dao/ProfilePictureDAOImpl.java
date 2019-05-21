5f05c53e25e061524acaa9f2485ec92b994c1010
sushovan47 <47148682+sushovan47@users.noreply.github.com>
2019-05-21 23:17:17 +0530

    Add files via upload
    profilePicture DAO and Implementation  interface and class has been uploaded

---
+++ Sushovan_Backend/dao/ProfilePictureDAOImpl.java
@@ -0,0 +1,37 @@
+package com.coll.dao;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.coll.model.ProfilePicture;
+
+@Repository("profilePictureDAO")
+@Transactional
+public class ProfilePictureDAOImpl implements ProfilePictureDAO
+{
+	@Autowired
+	SessionFactory sessionFactory;
+
+	@Override
+	public void save(ProfilePicture profilePicture)
+	{
+		Session session=sessionFactory.openSession();
+		session.saveOrUpdate(profilePicture);
+		session.flush();
+		session.close();
+	}
+
+	@Override
+	public ProfilePicture getProfilePicture(String username) 
+	{
+		Session session=sessionFactory.openSession();
+		ProfilePicture profilePicture=session.get(ProfilePicture.class,username);
+		session.close();
+		return profilePicture;
+	}
+
+}
